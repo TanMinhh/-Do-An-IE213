73f96005dfc4672fbcad3c3198eb8b2d53762d3e
Vitmuoi <127380825+Vitmuoi@users.noreply.github.com>
2025-04-06 22:37:36 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors');
 
 dotenv.config();
 
@@ -27,7 +28,7 @@ const storeRoute = require('./routes/store.route.js');
 const app = express();
 
 // Middleware
-app.use(express.json());
+app.use(cors());app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 app.get('/', (req, res) => {
