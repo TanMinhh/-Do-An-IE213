77f2cc3caa981b9185301475319bd81962deb7d0
Vitmuoi <127380825+Vitmuoi@users.noreply.github.com>
2025-04-06 21:26:30 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,6 @@ const storeRoute = require('./routes/store.route.js');
 const app = express();
 
 // Middleware
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
