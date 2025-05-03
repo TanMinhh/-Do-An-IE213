0b951618c69331b7d731aa306a34a22d192f136a
Vitmuoi <127380825+Vitmuoi@users.noreply.github.com>
2025-05-03 19:06:24 +0700

    Update order.route.js

--- backend/routes/order.route.js
+++ backend/routes/order.route.js
@@ -5,25 +5,18 @@ const {placeOrder, placeOrderStripe, verifyStripe, allOrders, userOrders, update
 const adminAuth = require('../middleware/adminAuth.js')
 const authUser = require('../middleware/auth.js')
 
-// Đặt hàng (COD)
 router.post("/place", authUser, placeOrder);
 
-// Đặt hàng (Stripe)
 router.post("/stripe", authUser, placeOrderStripe);
 
-// Xác minh thanh toán Stripe
 router.post("/verifyStripe", authUser, verifyStripe);
 
-// Lấy danh sách tất cả đơn hàng (admin)
-router.get("/list", adminAuth, allOrders);
+router.post("/list", adminAuth, allOrders);
 
-// Lấy danh sách đơn hàng của một người dùng cụ thể
-router.get("/orders", authUser, userOrders);
+router.post("/status", adminAuth, updateStatus);
 
-// Cập nhật trạng thái đơn hàng
-router.put("/status", adminAuth, updateStatus);
+router.post("/orders", authUser, userOrders);
 
-// Xóa đơn hàng
 router.delete("/:id", deleteOrder);
 
 module.exports = router;
